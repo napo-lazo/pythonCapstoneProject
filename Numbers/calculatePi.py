94ed06b3fe94a9afcbe8d739b124ab5bec583dc7
napo-lazo <napoleon.lazo@hotmail.com>
2019-12-10 17:56:01 -0600

    Calculates pi without comparing to constant

--- Numbers/calculatePi.py
+++ Numbers/calculatePi.py
@@ -1,18 +1,32 @@
 #Find PI to the Nth Digit - Enter a number and have the program generate π (pi) up to that many decimal places. 
 #Keep a limit to how far the program will go.
 #https://www.mathscareers.org.uk/article/calculating-pi/
-from math import pi
 from decimal import Decimal, ROUND_DOWN
 
-#gets the constant value of PI from the math module and creates a decimal object with this value
 base = Decimal(10)
-pi = Decimal(pi)
 piApproximation = Decimal(3)
 
-x = 2
+piApproximationString = str(piApproximation) + ".0"
+
+decimalIndex = 2
+listOfApproximations = [piApproximationString, piApproximationString, piApproximationString, piApproximationString, piApproximationString]
+numerator = 2
 
 isOdd = True
 stillCalculating = True
+
+def moveApproximationValues(piApproximation):
+    listOfApproximations[4] = listOfApproximations[3]
+    listOfApproximations[3] = listOfApproximations[2]
+    listOfApproximations[2] = listOfApproximations[1]
+    listOfApproximations[1] = listOfApproximations[0]
+    listOfApproximations[0] = str(piApproximation)
+
+def checkForConvergence():
+    for x in range(0, 4):
+        if not listOfApproximations[x][decimalIndex] == listOfApproximations[x + 1][decimalIndex]:
+            return False
+    return True
 
 #cycle to validate user input
 while True:
@@ -31,13 +45,19 @@ else:
 
 while stillCalculating:
     if isOdd:
-        piApproximation += Decimal(4)/(x * (x+1) * (x+2))
+        piApproximation += Decimal(4)/(numerator * (numerator+1) * (numerator+2))
+        moveApproximationValues(piApproximation)
         isOdd = False
     else:
-        piApproximation -= Decimal(4)/(x * (x+1) * (x+2))
+        piApproximation -= Decimal(4)/(numerator * (numerator+1) * (numerator+2))
+        moveApproximationValues(piApproximation)
         isOdd = True
-    x += 2
-    if piApproximation.quantize(Decimal(n), rounding=ROUND_DOWN) - pi.quantize(Decimal(n), rounding=ROUND_DOWN) == 0:
+    numerator += 2
+    
+    if checkForConvergence():
+        decimalIndex += 1
+
+    if decimalIndex - 2 == decimalPlaces:
         stillCalculating = False
 
 print(Decimal(piApproximation).quantize(Decimal(n), rounding=ROUND_DOWN))
